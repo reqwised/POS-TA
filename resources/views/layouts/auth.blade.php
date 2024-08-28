503b068c5c729d04e74f4267f8e82ecbf3f15511
Juha Arsya <arsyadani46@gmail.com>
2024-08-28 14:40:10 +0700

    link login

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name') }} | Login</title>
+    <title>{{ $setting->nama_perusahaan }} | Login</title>
     <link rel="icon" href="{{ url($setting->path_logo) }}" type="image/png">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('/AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
